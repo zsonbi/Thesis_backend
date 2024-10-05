74ef7ad70eab58485f0bb7f8227d8a7f6d95fdc3
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-10-05 22:23:01 +0200

    Removed ordering

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -55,7 +55,7 @@ namespace Thesis_backend.Controllers
 
             long loggedInUserId = (long)(this.GetLoggedInUser()!);
 
-            List<TaskHistory> taskHistories = await Database.TaskHistories.All.Include(t => t.CompletedTask).Where(x => x.OwnerId == loggedInUserId).OrderByDescending(x => x.Completed.Ticks).Take(Config.TASK_HISTORY_SIZE).ToListAsync();
+            List<TaskHistory> taskHistories = await Database.TaskHistories.All.Include(t => t.CompletedTask).Where(x => x.OwnerId == loggedInUserId).Take(Config.TASK_HISTORY_SIZE).ToListAsync();
 
             if (taskHistories is null)
             {
