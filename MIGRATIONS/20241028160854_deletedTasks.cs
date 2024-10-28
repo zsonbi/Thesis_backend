6a74648816d8160e71a12415acd760fbe236efc0
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-10-28 17:09:04 +0100

    migration...

---
+++ MIGRATIONS/20241028160854_deletedTasks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thesis_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletedTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "TasksTable",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "TasksTable");
+        }
+    }
+}
