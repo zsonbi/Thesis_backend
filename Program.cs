b931b03cff6f091b2bd3981608e8ce58b7d49dcd
zsonbi <zsomborder@gmail.com>
2025-02-12 19:48:36 +0200

    added logging to the backend

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 using Thesis_backend.Data_Structures;
 using Microsoft.EntityFrameworkCore;
-
+using System;
+using Serilog;
+using Serilog.AspNetCore;
+using Serilog.Debugging;
+using Serilog.Sinks.Graylog;
+using Microsoft.AspNetCore;
 namespace Thesis_backend
 {
     public class Program
@@ -9,6 +14,10 @@ namespace Thesis_backend
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            string domain = Environment.GetEnvironmentVariable("DOMAIN") ?? "localhost";
+            string graylog_ip = Environment.GetEnvironmentVariable("GRAYLOG_IP") ?? "none";
+            int graylog_port=0;
+            int.TryParse((Environment.GetEnvironmentVariable("GRAYLOG_PORT")) ?? "0",out graylog_port);
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -38,7 +47,7 @@ namespace Thesis_backend
                  options.AddPolicy("AllowTrusted", // This is the open house we talked about!
                      builder =>
                      {
-                         builder.WithOrigins("https://zsonbi.github.io/Thesis/game/", "https://zsonbi.github.io", "https://thesis.picidolgok.hu", "http://thesis.picidolgok.hu")
+                         builder.WithOrigins("https://zsonbi.github.io/Thesis/game/", "https://zsonbi.github.io", $"https://{domain}", $"http://{domain}")
                              .AllowAnyHeader() // With any type of headers...
                              .AllowAnyMethod() // And any HTTP methods. Such a jolly party indeed!
                              .AllowCredentials()
@@ -47,27 +56,53 @@ namespace Thesis_backend
                      });
              });
 
+
+            Console.WriteLine(graylog_ip);
+            Console.WriteLine(graylog_port);
+            // Configure Serilog with Graylog only in production
+            if (graylog_ip != "none" && graylog_port != 0)
+            {
+                Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Information() // Set the minimum log level
+                   .WriteTo.Console()
+                   .WriteTo.Graylog(new GraylogSinkOptions
+                   {
+                       HostnameOrAddress = graylog_ip, // Replace with your Graylog server address
+                       Port = graylog_port, // Default GELF UDP port
+                       TransportType = Serilog.Sinks.Graylog.Core.Transport.TransportType.Tcp,
+                       HostnameOverride="thesis_backend",
+
+                   })
+                   .CreateLogger();
+
+                builder.Host.UseSerilog();
+            }
+
             builder.Services.AddDbContext<ThesisDbContext>(options =>
             options.UseMySql(connectionStrings[0],
 
             new MySqlServerVersion(new Version(10, 5, 9))));
-            builder.Services.AddHttpsRedirection(options =>
+            if (domain != "localhost")
             {
-                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
-                options.HttpsPort = 8001;
-            });
+                builder.Services.AddHttpsRedirection(options =>
+                {
+                    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                    options.HttpsPort = 8001;
+                });
+            }
             var app = builder.Build();
 
             // Enable CORS globally
             app.UseCors("AllowTrusted");
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseSession();
-            app.UseHttpsRedirection();
+            if (domain != "localhost")
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseAuthorization();
 
