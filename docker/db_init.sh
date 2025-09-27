535cf30e16b44a50ff3b3683966152cb94109b6d
zsonbi <zsomborder@gmail.com>
2025-09-27 16:38:33 +0200

    Changed init to mariadb

--- docker/db_init.sh
+++ docker/db_init.sh
@@ -2,7 +2,7 @@
 set -e
 
 # This is the most reliable way to pass environment variables to the SQL commands.
-mysql -v -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
+mariadb -v -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
